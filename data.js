c998db6bbd8db53b2e9e1cf66ea1d59f8cd43607
dunestudios <info@dunestudios.io>
2026-08-22 17:20:22 +0200

    Dune 3D interactive 360 experience (Marzipano)

---
+++ data.js
@@ -0,0 +1,355 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-main",
+      "name": "Main",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.672616663858463,
+          "pitch": 0.18114269838410912,
+          "rotation": 0,
+          "target": "1-wrtw"
+        },
+        {
+          "yaw": 0.7380003660249947,
+          "pitch": 0.19940308129541506,
+          "rotation": 0,
+          "target": "7-silk"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wrtw",
+      "name": "WRTW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0131919311676931,
+          "pitch": 0.3542291695258424,
+          "rotation": 0,
+          "target": "2-wrtw2"
+        },
+        {
+          "yaw": 0.5520739548314602,
+          "pitch": 0.1940446680172272,
+          "rotation": 0,
+          "target": "4-hats-and-belts"
+        },
+        {
+          "yaw": 1.5375804854495136,
+          "pitch": 0.27663973921999485,
+          "rotation": 0,
+          "target": "7-silk"
+        },
+        {
+          "yaw": -2.6903101534084737,
+          "pitch": 0.16097157503958215,
+          "rotation": 0,
+          "target": "0-main"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-wrtw2",
+      "name": "WRTW2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5357162539779736,
+          "pitch": 0.11797069773805013,
+          "rotation": 0,
+          "target": "3-cashier"
+        },
+        {
+          "yaw": 2.0544300192870386,
+          "pitch": 0.16445435899428418,
+          "rotation": 0,
+          "target": "1-wrtw"
+        },
+        {
+          "yaw": 1.097135231133338,
+          "pitch": 0.08522275979923677,
+          "rotation": 0,
+          "target": "4-hats-and-belts"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cashier",
+      "name": "CASHIER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1119298557469453,
+          "pitch": 0.4067240553693985,
+          "rotation": 0,
+          "target": "2-wrtw2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hats-and-belts",
+      "name": "HATS AND BELTS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.086546630917205,
+          "pitch": 0.06486086894943455,
+          "rotation": 0,
+          "target": "5-shoes"
+        },
+        {
+          "yaw": 2.8216517542992783,
+          "pitch": 0.13814106788153246,
+          "rotation": 0,
+          "target": "7-silk"
+        },
+        {
+          "yaw": -2.590047665425299,
+          "pitch": 0.23857046488674705,
+          "rotation": 0,
+          "target": "1-wrtw"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-shoes",
+      "name": "SHOES",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.165257251350706,
+          "pitch": 0.24809308287007248,
+          "rotation": 0,
+          "target": "4-hats-and-belts"
+        },
+        {
+          "yaw": -3.1296630731157187,
+          "pitch": 0.28177393794334904,
+          "rotation": 0,
+          "target": "6-leather"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-leather",
+      "name": "LEATHER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05786494846430834,
+          "pitch": 0.31361448300077654,
+          "rotation": 0,
+          "target": "5-shoes"
+        },
+        {
+          "yaw": -2.2699683052582547,
+          "pitch": 0.2504096732983676,
+          "rotation": 0,
+          "target": "7-silk"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-silk",
+      "name": "SILK",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13677562374706298,
+          "pitch": 0.2625034108922577,
+          "rotation": 0,
+          "target": "4-hats-and-belts"
+        },
+        {
+          "yaw": 0.7295016632539362,
+          "pitch": 0.33120619542151886,
+          "rotation": 0,
+          "target": "6-leather"
+        },
+        {
+          "yaw": -2.5186373454109834,
+          "pitch": 0.2359785267779415,
+          "rotation": 0,
+          "target": "0-main"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
